42d571c0a62dc34bc8e1bba0b450cbc513eb07d4
Ozee <ozee@github.com>
2018-10-30 20:11:11 +0530

    marquee

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -1,5 +1,19 @@
 ( function($) {
-	$('.js-marquee').marquee();
+
+	$('.marquee').marquee({
+		//speed in milliseconds of the marquee in milliseconds
+		duration: 15000,
+		//gap in pixels between the tickers
+		gap: 0,
+		//time in milliseconds before the marquee will start animating
+		delayBeforeStart: 0,
+		//'left' or 'right'
+		direction: 'left',
+		//true or false - should the marquee be duplicated to show an effect of continues flow
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible: true
+	});
 
 	$( '.zoom' ).on( {
 		mouseover: function () {
